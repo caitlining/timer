344e1d6c68950d2d1ac80c6674960bb869d413dd
caitlining <caitlincing@gmail.com>
2019-09-24 21:13:37 +0000

    Submitting timer function, pair programmed with @Justin_Lighthouse

---
+++ timer1.js
@@ -0,0 +1,21 @@
+let alarmTimeArray = process.argv.slice(2);
+
+const alarmFunc = function (timesArr) {
+
+  if (timesArr.length === 0) {
+    return;
+  }
+
+  for (const time of timesArr) {
+    let alarmTime = Number(time);
+    if (Number.isFinite(alarmTime) && alarmTime >= 0) {
+      console.log(alarmTime);
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, alarmTime);
+    }
+  }
+};
+
+alarmFunc(alarmTimeArray);
+
